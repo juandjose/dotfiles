a9a55dff36db95d8d3c8eef494aacdf92d369663
bby <100054443+juandjose@users.noreply.github.com>
2023-11-07 18:22:04 -0500

    Add files via upload

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,117 +1,18 @@
-local opts = { noremap = true, silent = true }
-local silent  = { silent = true }
-local noremap = { noremap = true}
-local expr = { expr = true }
+vim.g.mapleader = ' '
 
-local keymap = vim.api.nvim_set_keymap
+vim.keymap.set('n', '<leader>c', '<cmd>let @/=""<CR>', noremap)
 
-vim.g.EasyMotion_do_mapping = 0
-vim.g.EasyMotion_smartcase = 1
+vim.keymap.set({'n', 'v'}, 'x', '"_x')
+vim.keymap.set({'n', 'v'}, 's', '"_s')
 
-keymap('n', '<leader>c', '<cmd>let @/=""<CR>', noremap)
+vim.keymap.set('n', '<c-d>', '<c-d>zz')
+vim.keymap.set('n', '<c-u>', '<c-u>zz')
 
--- easy navigation
-keymap('n', 'ó', '<cmd>e ~/Appdata/Local/nvim/lua/options.lua<CR>', opts)
-keymap('n', '´k', '<cmd>e ~/Appdata/Local/nvim/lua/keymaps.lua<CR>', opts)
-keymap('n', 'á', '<cmd>e ~/Appdata/Local/nvim/lua/autocmds.lua<CR>', opts)
-keymap('n', '´p', '<cmd>e ~/Appdata/Local/nvim/lua/plugins.lua<CR>', opts)
-keymap('n', '´P', '<cmd>e ~/Appdata/Local/nvim/plugin/<CR>', opts)
+vim.keymap.set('n', 'q:', '<nop>', opts)
+vim.keymap.set('n', 'Q', '<nop>', opts)
+vim.keymap.set('n', '<c-z>', '<nop>', opts)
 
--- quick cmd
-keymap('n', ';', ':', noremap)
-keymap('n', ':', '.', noremap)
-keymap('n', '.', ';', noremap)
-keymap('n', '<CR>', '.', noremap)
-keymap('v', ';', ':', noremap)
-keymap('v', '<CR>', '.', noremap)
-keymap('v', '.', ';', noremap)
-
--- windows
-keymap('n', '<c-h>', '<c-w>h', opts)
-keymap('n', '<c-j>', '<c-w>j', opts)
-keymap('n', '<c-k>', '<c-w>k', opts)
-keymap('n', '<c-l>', '<c-w>l', opts)
-keymap('n', '<leader>sv', '<cmd>vsplit<CR><c-w>w<c-w>=', opts)
-keymap('n', '<leader>ss', '<cmd>split<CR><c-w>w<c-w>=', opts)
-keymap('n', '<leader>st', '<c-w>T', opts)
-keymap('n', '<leader>se', '<c-w>=', opts)
-keymap('n', '<leader>so', '<c-w>o', opts)
-
--- tabs
-keymap('v', '<TAB>', '>gv_', opts)
-keymap('v', '<S-TAB>', '<gv_', opts)
-
--- save
-keymap('i', '<c-s>', '<c-o><cmd>update<CR>', opts)
-
--- select-all
-keymap('n', '<c-a>', 'ggVG$', opts)
-keymap('v', '<c-a>', '<esc>ggVG$', opts)
-
--- copy & cut
-keymap('v', '<c-c>', '"+y', opts)
-keymap('v', '<c-x>', '"+x', opts)
-keymap('i', '<c-c>', '<c-o>"+yy', opts)
-
--- paste
-keymap('n', 'p', 'gp', opts)
-keymap('n', 'P', 'gP', opts)
-
--- yankn't
-keymap('n', 'x', '"_x', opts)
-keymap('n', 's', '"_s', opts)
-keymap('v', 'x', '"_x', opts)
-keymap('v', 's', '"_s', opts)
-keymap('v', '<bs>', '"_x', opts)
-
--- aument & decrement
-keymap('n', '+', '<c-a>', opts)
-keymap('n', '-', '<c-x>', opts)
-
--- move line
-keymap('n', 'J', '<cmd>m .+1<CR>==', opts)
-keymap('n', 'K', '<cmd>m .-2<CR>==', opts)
-keymap('v', 'J', ":m '>+1<CR><CR>gv=gv", opts)
-keymap('v', 'K', ":m '<-2<CR><CR>gv=gv", opts)
-
--- disable
-keymap('n', 'q:', '<nop>', opts)
-keymap('n', 'Q', '<nop>', opts)
-keymap('n', '<bs>', '<nop>', opts)
-keymap('n', '<c-z>', '<nop>', opts)
-keymap('v', '<c-z>', '<nop>', opts)
-keymap('n', '<space>', '<nop>', opts)
-keymap('i', '<c-a>', '<nop>', opts);
-
--- easymotion move to line
-keymap('n', 'gj', '<Plug>(easymotion-j)', opts)
-keymap('n', 'gk', '<Plug>(easymotion-k)', opts)
-keymap('v', 'gj', '<Plug>(easymotion-j)', opts)
-keymap('v', 'gk', '<Plug>(easymotion-k)', opts)
--- easymotion move to word
-keymap('n', '<leader>w', '<Plug>(easymotion-w)', opts)
-keymap('n', '<leader>b', '<Plug>(easymotion-b)', opts)
-keymap('v', '<leader>w', '<Plug>(easymotion-w)', opts)
-keymap('v', '<leader>b', '<Plug>(easymotion-b)', opts)
-
--- o-pending quote
-keymap('o', 'iq', 'i"', opts)
-keymap('o', 'aq', 'a"', opts)
-keymap('v', 'iq', 'i"', opts)
-keymap('v', 'aq', 'a"', opts)
--- o-pending curly braces
-keymap('o', 'ic', 'i{', opts)
-keymap('o', 'ac', 'a{', opts)
-keymap('v', 'ic', 'i{', opts)
-keymap('v', 'ac', 'a{', opts)
-
--- better-search
-keymap('n', 'n', 'nzz', opts)
-keymap('n', 'N', 'Nzz', opts)
-
--- better-scrolling
-keymap('n', '<c-u>', '<c-u>zz', opts)
-keymap('n', '<c-d>', '<c-d>zz', opts)
-
-keymap('v', '<c-u>', '<c-u>zz', opts)
-keymap('v', '<c-d>', '<c-d>zz', opts)
+vim.cmd[[
+    noremap! <C-BS> <C-w>
+    noremap! <C-h> <C-w>
+]]
--- lua/options.lua
+++ lua/options.lua
@@ -1,56 +1,28 @@
-local opt = vim.opt
-local g = vim.g
+vim.opt.number = true
+vim.opt.relativenumber = true
 
-g.mapleader = ' '
+vim.opt.cursorline = true
+vim.opt.guicursor ='n-v-c-sm:blinkon100,i-ci-ve:blinkon100,r-cr-o:hor20'
 
--- numbers
-opt.number = true
+vim.opt.termguicolors = true
+vim.opt.wrap = false
+vim.opt.signcolumn = 'yes:1'
+vim.opt.clipboard = 'unnamed'
+vim.opt.background = 'dark'
+vim.opt.updatetime = 100
+vim.opt.fillchars = { eob = " "}
 
--- wrap
-opt.wrap = false
-opt.whichwrap:append "<>[]hl"
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
 
--- scroll
--- opt.scrolloff = 10
--- opt.sidescrolloff = 10
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
--- signcolumn
-opt.signcolumn = 'yes:1'
-opt.fillchars = { eob = " "}
-
--- cursor
-opt.cursorline = true
-opt.guicursor ='n-v-c-sm:blinkon100,i-ci-ve:blinkon100,r-cr-o:hor20'
-
--- statusline
-opt.ruler = false
-opt.laststatus = 3
-
--- command
--- opt.cmdheight = 0
-opt.showmode = false
-opt.cmdwinheight = 5
-
--- other
-opt.clipboard = 'unnamed'
-opt.mouse = ''
-opt.updatetime = 100
--- opt.undofile = true
-
--- indent
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.shiftwidth = 4
-opt.expandtab = true
-opt.smartindent = true
-
--- search
-opt.hlsearch = true
-opt.incsearch = true
-opt.ignorecase = true
-opt.smartcase = true
-
--- colorscheme
-opt.termguicolors = true
-opt.background = 'dark'
-
+vim.cmd[[
+    colorscheme habamax
+]]
